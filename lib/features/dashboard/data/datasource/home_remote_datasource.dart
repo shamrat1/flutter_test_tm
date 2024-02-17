d2882a4534d286f582176359f085b52d6c6f4c08
shamrat1 <yshamrat@gmail.com>
2024-02-17 15:42:25 +0600

    update initial setup

---
+++ lib/features/dashboard/data/datasource/home_remote_datasource.dart
@@ -0,0 +1,73 @@
+// abstract class DashboardDatasource {
+//   Future<Either<AppException, PaginatedResponse>> fetchPaginatedProducts(
+//       {required int skip});
+//   Future<Either<AppException, PaginatedResponse>> searchPaginatedProducts(
+//       {required int skip, required String query});
+// }
+
+// class DashboardRemoteDatasource extends DashboardDatasource {
+//   final NetworkService networkService;
+//   DashboardRemoteDatasource(this.networkService);
+
+//   @override
+//   Future<Either<AppException, PaginatedResponse>> fetchPaginatedProducts(
+//       {required int skip}) async {
+//     final response = await networkService.get(
+//       '/products',
+//       queryParameters: {
+//         'skip': skip,
+//         'limit': PRODUCTS_PER_PAGE,
+//       },
+//     );
+
+//     return response.fold(
+//       (l) => Left(l),
+//       (r) {
+//         final jsonData = r.data;
+//         if (jsonData == null) {
+//           return Left(
+//             AppException(
+//               identifier: 'fetchPaginatedData',
+//               statusCode: 0,
+//               message: 'The data is not in the valid format.',
+//             ),
+//           );
+//         }
+//         final paginatedResponse =
+//             PaginatedResponse.fromJson(jsonData, jsonData['products'] ?? []);
+//         return Right(paginatedResponse);
+//       },
+//     );
+//   }
+
+//   @override
+//   Future<Either<AppException, PaginatedResponse>> searchPaginatedProducts(
+//       {required int skip, required String query}) async {
+//     final response = await networkService.get(
+//       '/products/search?q=$query',
+//       queryParameters: {
+//         'skip': skip,
+//         'limit': PRODUCTS_PER_PAGE,
+//       },
+//     );
+
+//     return response.fold(
+//       (l) => Left(l),
+//       (r) {
+//         final jsonData = r.data;
+//         if (jsonData == null) {
+//           return Left(
+//             AppException(
+//               identifier: 'search PaginatedData',
+//               statusCode: 0,
+//               message: 'The data is not in the valid format.',
+//             ),
+//           );
+//         }
+//         final paginatedResponse =
+//             PaginatedResponse.fromJson(jsonData, jsonData['products'] ?? []);
+//         return Right(paginatedResponse);
+//       },
+//     );
+//   }
+// }
---
+++ lib/shared/data/remote/network_service.dart
@@ -0,0 +1,20 @@
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+
+// abstract class NetworkService {
+//   String get baseUrl;
+
+//   Map<String, Object> get headers;
+
+//   void updateHeader(Map<String, dynamic> data);
+
+//   Future<Either<AppException, Response>> get(
+//     String endpoint, {
+//     Map<String, dynamic>? queryParameters,
+//   });
+
+//   Future<Either<AppException, Response>> post(
+//     String endpoint, {
+//     Map<String, dynamic>? data,
+//   });
+// }
